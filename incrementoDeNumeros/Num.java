1b3bca468542478aea331586604eca8aae3abea6
RodriCA23 <111400739+RodriCA23@users.noreply.github.com>
2022-10-29 20:10:12 -0500

    Add files via upload

---
+++ incrementoDeNumeros/Num.java
@@ -0,0 +1,20 @@
+
+package inc;
+
+import javax.swing.JOptionPane;
+
+
+public class Num {
+
+   
+    public static void main(String[] args) {
+        /*int i=10;
+        JOptionPane.showMessageDialog(null, "Imprimir numeros del 10 al 1 con while");
+        while (i>=1){
+        System.out.println(i);
+        i--;
+        }*/
+       
+    }
+    
+}
