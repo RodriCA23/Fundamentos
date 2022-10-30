8eec3a02d5a06b8ec8a9dd5c0c76eea2519f929a
RodriCA23 <111400739+RodriCA23@users.noreply.github.com>
2022-10-29 20:09:43 -0500

    Add files via upload

---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,16 @@
+
+package HolaMundo;
+
+import javax.swing.JOptionPane;
+
+
+public class HolaMundo {
+
+    
+    public static void main(String[] args) {
+       //System.out.println("Hola Mundo");
+       JOptionPane.showMessageDialog(null, "Hola Mundo");
+    }
+    
+    
+}
